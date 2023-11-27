3e52ea9143e13e607cddf234d0ed6940d0e314ed
巳午未申 <315578808@qq.com>
2023-11-27 19:34:39 +0800

    新建 vue-shop 后台管理

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
